ba7820a8fa12b0c9cd91705cd63997c94c321097
tiene123 <nguyentien0623@gmail.com>
2023-11-28 11:41:12 +0700

    tien

---
+++ admin/class/lienhe_class.php
@@ -0,0 +1,50 @@
+<?php
+include "database.php";
+
+?>
+<?php
+class LienHe
+{
+    private $db;
+    public function __construct()
+    {
+        $this->db = new Database();
+    }
+    public function insertlh($tendanhmuc)
+    {
+        $query = "INSERT INTO loaixe(tendanhmuc) VALUE ('$tendanhmuc')";
+        $result = $this->db->insert($query);
+        header('location:listdanhmuc.php');
+        return $result;
+    }
+    public function showlh()
+    {
+        $query = "SELECT * FROM lienhe ";
+        $result = $this->db->select($query);
+        return $result;
+    }
+    public function get_lienhe($lienhe_id)
+    {
+        $query = "SELECT * FROM lienhe WHERE lienhe_id = $lienhe_id";
+        $result = $this->db->select($query);
+        return $result;
+    }
+    public function updatelh($cartegory_id, $tendanhmuc)
+    {
+        $query = "UPDATE loaixe SET tendanhmuc = '$tendanhmuc' WHERE cartegory_id='$cartegory_id'";
+        $result = $this->db->update($query);
+        header('location:listdanhmuc.php');
+        return $result;
+
+    }
+    public function deletelh($lienhe_id)
+    {
+        $query = "DELETE FROM lienhe WHERE lienhe_id='$lienhe_id'";
+        $result = $this->db->delete($query);
+        header("location:listlienhe.php");
+        return $result;
+    }
+
+
+}
+?>
